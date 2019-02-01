771f3f83b89949d52741c442289fdba8ee55c4c8
ice-bit <ceticamarco@gmail.com>
2019-02-01 15:53:17 +0100

    Fixed some bugs

--- sources/path.cpp
+++ sources/path.cpp
@@ -3,28 +3,33 @@
 path::path() { this->user_path = nullptr; }
 
 QString path::get_path() {
-    this->user_path = QFileDialog::getOpenFileName(this,
+    QFileDialog dialog(this);
+    dialog.setDefaultSuffix(".db");
+    this->user_path = dialog.getOpenFileName(this,
         tr("Open an existing database"), "",
         tr("Database files (*.db)"));
     return this->user_path;
 }
 
 QString path::set_path() {
-    this->user_path = QFileDialog::getSaveFileName(this,
-        tr("Save a new database"), "",
-        tr("Database file (*.db)"));
-    #if OS == 0 // If the platform is an UNIX-like system we may need to add
-        user_path += ".db"; // the extension manually
+    QFileDialog dialog(this);
+    dialog.setDefaultSuffix(".db");
+    this->user_path = dialog.getSaveFileName(this,
+        tr("Save an new database"), "",
+        tr("Database files (*.db)"));
+    #if OS == 0 // If platform is Linux add file extension manually
+        this->user_path += ".db";
     #endif
     return this->user_path;
 }
 
 QString path::set_path_pdf() {
-    this->user_path = QFileDialog::getSaveFileName(this,
+    QFileDialog dialog(this);
+    this->user_path = dialog.getSaveFileName(this,
         tr("Save a new pdf"), "",
         tr("PDF File (*.pdf)"));
-    #if OS == 0 // If the platform is an UNIX-like system we may need to add
-        user_path += ".pdf"; // the extension manually
+    #if OS == 0 // If platform is Linux add file extension manually
+        this->user_path += ".db";
     #endif
     return this->user_path;
 }
